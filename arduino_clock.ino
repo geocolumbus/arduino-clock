15d74b5092ecc6fb6ccc050ebd08e8b19ce45388
George Campbell <geocolumbus@gmail.com>
2020-02-08 20:56:31 -0500

    Arduino getting a handle on pixel fonts

--- arduino_clock.ino
+++ arduino_clock.ino
@@ -1,15 +1,35 @@
 #include <Arduino.h>
 #include "font6x8/font6x8.h"
+#include <string.h>
 
 void setup() {
     Serial.begin(9600);
 }
 
-void loop() {
+void drawCharacterLine(unsigned char lineValue /*, row, colOffset */) {
+    char line[14] = "";
+    unsigned char a = lineValue;
+    for (int col = 0; col < 6; col++) {
+        if ((a & 64) > 0) {
+            strcat(line, "* "); // set(row,col+colOffset)
+        } else {
+            strcat(line, "  ");
+        }
+        a = a << 1;
+    }
+    Serial.println(line);
+}
 
-    Serial.print((int)font[0]);
-    Serial.print(" ");
-    Serial.print((int)font[1]);
-    Serial.print(" ");
-    delay(1000);
+void drawCharacter(int ascii /*, colOffset */) {
+    //char buffer[7];
+    for (int row = 0; row < 8; row++) {
+        drawCharacterLine(font[(ascii - 32) * 8 + row]);
+    }
+}
+
+void loop() {
+    Serial.println("");
+    drawCharacter((int)'G');
+    drawCharacter((int)'e');
+    delay(20000);
 }
--- cmake-build-debug/arduino_clock_arduino_clock.ino.cpp
+++ cmake-build-debug/arduino_clock_arduino_clock.ino.cpp
@@ -2,27 +2,52 @@
 #line 1 "/Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino"
 #include <Arduino.h>
 #include "font6x8/font6x8.h"
+#include <string.h>
 
-#line 6 "/Users/georgecampbell/arduino/arduino-clock/cmake-build-debug/arduino_clock_arduino_clock.ino.cpp"
+#line 7 "/Users/georgecampbell/arduino/arduino-clock/cmake-build-debug/arduino_clock_arduino_clock.ino.cpp"
 #include "Arduino.h"
 
 //=== START Forward: /Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino
  void setup() ;
  void setup() ;
+ void drawCharacterLine(unsigned char lineValue ) ;
+ void drawCharacterLine(unsigned char lineValue ) ;
+ void drawCharacter(int ascii ) ;
+ void drawCharacter(int ascii ) ;
  void loop() ;
  void loop() ;
 //=== END Forward: /Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino
-#line 2 "/Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino"
+#line 3 "/Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino"
 
 
 void setup() {
     Serial.begin(9600);
 }
 
+void drawCharacterLine(unsigned char lineValue /*, row, colOffset */) {
+    char line[14] = "";
+    unsigned char a = lineValue;
+    for (int col = 0; col < 6; col++) {
+        if ((a & 64) > 0) {
+            strcat(line, "* "); // set(row,col+colOffset)
+        } else {
+            strcat(line, "  ");
+        }
+        a = a << 1;
+    }
+    Serial.println(line);
+}
+
+void drawCharacter(int ascii /*, colOffset */) {
+    //char buffer[7];
+    for (int row = 0; row < 8; row++) {
+        drawCharacterLine(font[(ascii - 32) * 8 + row]);
+    }
+}
+
 void loop() {
-    Serial.print((int)font[0]);
-    Serial.print(" ");
-    Serial.print((int)font[1]);
-    Serial.print(" ");
-    delay(1000);
+    Serial.println("");
+    drawCharacter((int)'G');
+    drawCharacter((int)'e');
+    delay(20000);
 }
