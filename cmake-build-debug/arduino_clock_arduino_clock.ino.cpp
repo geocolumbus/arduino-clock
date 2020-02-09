df8da973dd6c6e7f1376b3d85151f3171f849aab
George Campbell <geocolumbus@gmail.com>
2020-02-08 21:18:56 -0500

    Getting ready to lite LED's

--- cmake-build-debug/arduino_clock_arduino_clock.ino.cpp
+++ cmake-build-debug/arduino_clock_arduino_clock.ino.cpp
@@ -10,10 +10,12 @@
 //=== START Forward: /Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino
  void setup() ;
  void setup() ;
- void drawCharacterLine(unsigned char lineValue ) ;
- void drawCharacterLine(unsigned char lineValue ) ;
- void drawCharacter(int ascii ) ;
- void drawCharacter(int ascii ) ;
+ void drawCharacterLine(unsigned char lineValue, int row, int colOffset) ;
+ void drawCharacterLine(unsigned char lineValue, int row, int colOffset) ;
+ void drawCharacter(int ascii, int colOffset) ;
+ void drawCharacter(int ascii, int colOffset) ;
+ void drawWord(char *word) ;
+ void drawWord(char *word) ;
  void loop() ;
  void loop() ;
 //=== END Forward: /Users/georgecampbell/arduino/arduino-clock/arduino_clock.ino
@@ -24,30 +26,35 @@ void setup() {
     Serial.begin(9600);
 }
 
-void drawCharacterLine(unsigned char lineValue /*, row, colOffset */) {
+void drawCharacterLine(unsigned char lineValue, int row, int colOffset) {
     char line[14] = "";
+    char buffer[32];
     unsigned char a = lineValue;
     for (int col = 0; col < 6; col++) {
         if ((a & 64) > 0) {
             strcat(line, "* "); // set(row,col+colOffset)
+            sprintf(buffer, "(%d, %2d)", row, col + colOffset);
+            Serial.println(buffer);
         } else {
             strcat(line, "  ");
         }
         a = a << 1;
     }
-    Serial.println(line);
 }
 
-void drawCharacter(int ascii /*, colOffset */) {
-    //char buffer[7];
+void drawCharacter(int ascii, int colOffset) {
     for (int row = 0; row < 8; row++) {
-        drawCharacterLine(font[(ascii - 32) * 8 + row]);
+        drawCharacterLine(font[(ascii - 32) * 8 + row], row, colOffset);
+    }
+}
+
+void drawWord(char *word) {
+    for (int i = 0; i < 5; i++) {
+        drawCharacter((int) word[i], i * 7);
     }
 }
 
 void loop() {
-    Serial.println("");
-    drawCharacter((int)'G');
-    drawCharacter((int)'e');
-    delay(20000);
+    drawWord("12:34");
+    delay(60000);
 }
